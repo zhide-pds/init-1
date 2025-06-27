c5d38482b40c5331eb3da91a65734ff4f79d764b
zhide-pds <zhide.loh@pdsvision.com>
2025-06-27 11:57:13 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+
+# Make sure the script is not run as root.
+if [ $(id -u) -eq 0 ]; then
+    echo
+    echo
+    echo "Please do not run as root."
+    exit 1
+fi
+
+# Make sure Node-RED is already installed.
+nodered_dir="/home/pi/.node-red"
+ls $nodered_dir >/dev/null 2>/dev/null
+if [ $? -ne 0 ]; then
+    echo 'Please install Node-RED before running this script.'
+	exit 1
+fi
+
+# Download Iptables
+echo
+echo
+echo "#################"
+echo "Downloading and setting up port forwarding"
+echo "#################"
+echo
+
+curl -LO https://raw.githubusercontent.com/zhide-pds/init-1/refs/heads/main/iptable.txt
+sudo apt-get install iptables-persistent
+sudo mv iptables.txt /etc/iptables/rules.v4
+curl -LO https://raw.githubusercontent.com/zhide-pds/init-1/refs/heads/main/sysctl.conf
+sudo mv sysctl.conf /etc/sysctl.conf
+
+
+# Install cert
+echo
+echo
+echo "#################"
+echo "Downloading and installing cert"
+echo "#################"
+echo
+
+curl -LO https://raw.githubusercontent.com/zhide-pds/init-1/refs/heads/main/chosen.crt
+cd /usr/share/ca-certificates
+sudo cp /home/pi/chosen.crt amead_ca.crt
+sudo dpkg-reconfigure ca-certificates
+
+# Install Node-RED nodes.
+echo
+echo
+echo "################"
+echo "Installing Nodes"
+echo "################"
+echo
+
+cd $nodered_dir
+npm install node-red-contrib-opcua-server
+npm install node-red-contrib-opcua-server-refresh
+npm install node-red-contrib-opcua
+npm install node-red-omronplc
+npm install node-red-contrib-omron-fins
+npm install node-red-show-value
+npm install node-red-contrib-modbus
+npm install @serafintech/node-red-contrib-eip-io
+npm install node-red-contrib-schneider-powerlogic
+npm install node-red-contrib-network-tools
+npm install node-red-contrib-thingrest
+npm install node-red-contrib-edge-trigger
+npm install node-red-contrib-persistent-global-context
+
+
+
+cat /sys/class/net/wlan0/address
+
+echo
+echo
+echo "#####################################"
+echo "PDS Cytron MCA Setup Completed"
+echo "#####################################"
+echo
+echo "Please reboot for the changes to take effect."
+echo "It will take some times to reboot."
